2a8e093b1a365ce6c2c619d29283ac8c05fda5cf
Adrian Wiendl <s83770@htw-dresden.de>
2023-01-17 01:44:49 +0100

    Minor Changes

--- Windows/src_new/sim_errors.h
+++ Windows/src_new/sim_errors.h
@@ -12,7 +12,7 @@ int provokeSeqError(int currentPacket, int packetToSkip) // SENDER
     // Provoke erroneous sequencing on 5th packet
     if (currentPacket == packetToSkip && seqRetries < 1) // skip packet packetToSkip
     {
-        printf("Forcibly skipping packet %d.\n", packetToSkip);
+        printf("Forcibly skipping packet [%d].\n\n", packetToSkip);
         seqRetries++;
         currentPacket++;
     }
@@ -25,7 +25,7 @@ int provokeChecksumError(int currentPacket, int checksum, int packetToFalsify) /
     // Provoke erroneous checksum on 7th packet
     if (currentPacket == packetToFalsify && csmRetries < 1)
     {
-        printf("Forcibly falsifying checksum. (-1)\n");
+        printf("Forcibly falsifying checksum. (Checksum -= 1)\n\n");
         checksum--; // falsify checksum by changing value
         csmRetries++;
     }
@@ -38,7 +38,7 @@ int provokeMissingAck(int seqNr, int seqNrToSkip) // RECEIVER
     time_t start, end;
     if (seqNr == seqNrToSkip && ackRetries < 1)
     {
-        printf("Forcing missing acknowledgement on packet %d.\n", seqNrToSkip);
+        printf("Forcing missing acknowledgement on packet with seq-nr [%d].\n\n", seqNrToSkip);
         ackRetries++;
         //Sleep(6000); // longer than TIMEOUT (5s)
         /* wait 5 seconds */
